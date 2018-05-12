6712c81fcb2c7a84321512ad9c708e44e20e4838
Chidozie Nnachor <odogwu.naija@gmail.com>
2018-05-12 22:30:49 +0000

    Add account activation

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -34,9 +34,9 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     log_in_as(@other_user)
     assert_not @other_user.admin?
     patch user_path(@other_user), params: {
-                                    user: { password:              "",
-                                            password_confirmation: "",
-                                            admin: true } }
+                                    user: { password: "",
+                                    password_confirmation: "",
+                                    admin: true } }
     assert_not @other_user.reload.admin?
   end
   
